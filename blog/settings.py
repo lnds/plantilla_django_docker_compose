ffdcb17b8f40cf5d176c344fff44cffa1a7cba7d
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-04 11:43:21 -0400

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -143,9 +143,3 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-    ('admin', 'admin@mysite.com'),
-)
-
-DEFAULT_ADMIN_PASSWORD= env('DEBUG', default='changeme')
